08f6f11859d1cc29b78e5130b8979ccfc5d78f7d
こちやち <kotiyatiq@gmail.com>
2025-11-21 13:59:30 +0900

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-alert("Hello, World!");
-document.getElementById("hello-btn").addEventListener("click",()=>{
-    alert("ぼたんが反応したね!");
-}); 
+const images = [
+    "./image/photo-1471922694854-ff1b63b20054.jpg",
+    "./image/p1.jpg",
+    "./image/p2.jpg",
+    "./image/p3.jpg"
+];
+
+let currentIndex = 0;
+const hero = document.querySelector(".hero");
+
+hero.style.backgroundImage =  `url(${images[currentIndex]})`;
+
+setInterval(() =>{
+    currentIndex = (currentIndex +1)% images.length;
+    hero.style.backgroundImage = `url(${images[currentIndex]})`;
+},5000);
